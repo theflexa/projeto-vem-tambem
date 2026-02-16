df8fbfc042e6575ea1ef576cfff4a10cce5898b4
Flexa <the.flexa@outlook.com>
2026-02-15 22:07:50 -0300

    chore: switch Docker config to dev database
    Development environment now points to sergi6131_banco_dados_dev
    instead of prod, allowing safe local testing with copied data.

--- src/main/java/br/com/vemtambem/model/Conexao.java
+++ src/main/java/br/com/vemtambem/model/Conexao.java
@@ -6,7 +6,7 @@ public class Conexao {
 	// public static final String URL = "jdbc:mysql://localhost:3306/sergi6131_banco_dados_dev";
 
 	// === LOCAL/DOCKER (descomentar para desenvolvimento) ===
-	public static final String URL = "jdbc:mysql://sergi6131.c44.integrator.host:3306/sergi6131_banco_dados_prod";
+	public static final String URL = "jdbc:mysql://sergi6131.c44.integrator.host:3306/sergi6131_banco_dados_dev";
 
 	public static final String USUARIO = "sergi6131_user";
 	public static final String SENHA = "FK(-@(25(I@LIGP~";
